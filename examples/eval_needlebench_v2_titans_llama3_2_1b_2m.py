67aa6b2721aa7c6a6f5177be2d4733b9730722f8
1120223075 <ericheffp@163.com>
2025-12-23 16:26:53 +0800

    添加titans2M

---
+++ examples/eval_needlebench_v2_titans_llama3_2_1b_2m.py
@@ -0,0 +1,37 @@
+from mmengine.config import read_base
+from opencompass.models import HuggingFaceBaseModel
+import os
+
+# Override via env var if needed, default is non-LoRA Titans-style delta_product.
+TPTT_SUBFOLDER = os.getenv('TPTT_SUBFOLDER', 'delta_product_m0.5_constant')
+
+with read_base():
+    from opencompass.configs.datasets.needlebench_v2.needlebench_v2_2m.needlebench_v2_2m import needlebench_datasets as datasets  # noqa: E501
+    from opencompass.configs.summarizers.needlebench_v2_2m_summarizer import needlebench_v2_2m_summarizer as summarizer  # noqa: E501
+
+models = [
+    dict(
+        type=HuggingFaceBaseModel,
+        abbr=f'titans-llama-3.2-1b-{TPTT_SUBFOLDER}',
+        path='ffurfaro/Titans-Llama-3.2-1B',
+        model_kwargs=dict(
+            device_map='auto',
+            trust_remote_code=True,
+            subfolder=TPTT_SUBFOLDER,
+            torch_dtype='torch.bfloat16',
+            max_self_attn_length=4096,
+            attn_implementation='flash_attention_2',
+        ),
+        tokenizer_kwargs=dict(
+            trust_remote_code=True,
+            subfolder=TPTT_SUBFOLDER,
+            padding_side='left',
+        ),
+        max_seq_len=2048000,
+        max_out_len=256,
+        batch_size=1,
+        run_cfg=dict(num_gpus=1),
+    )
+]
+
+work_dir = './outputs/needlebench_2m_titans_llama3_2_1b'
